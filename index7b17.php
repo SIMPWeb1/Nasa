da03086543dd3cc6dc773202ff451e5d0f81fd3b
Phong <phong@sss.local>
2023-09-27 16:25:07 +0700

    Done

---
+++ index7b17.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-gb">
+	<title type="text">HOBBES</title>
+	<subtitle type="text">Are you ready to embark on a crypto journey that's as wild as Elon Musk's Twitter feed? Introducing Hobbes Token, the cryptocurrency that takes its inspiration </subtitle>
+	<link rel="alternate" type="text/html" href="https://hobbestoken.vip"/>
+	<id>https://hobbestoken.vip/index.php</id>
+	<updated>2023-09-27T08:23:57+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://hobbestoken.vip/index.php?format=feed&amp;type=atom"/>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>HOBBES</title>
+		<description><![CDATA[Are you ready to embark on a crypto journey that's as wild as Elon Musk's Twitter feed? Introducing Hobbes Token, the cryptocurrency that takes its inspiration ]]></description>
+		<link>https://hobbestoken.vip/index.php</link>
+		<lastBuildDate>Wed, 27 Sep 2023 08:23:56 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="https://hobbestoken.vip/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+	</channel>
+</rss>
